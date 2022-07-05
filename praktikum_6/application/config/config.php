efc39ca3aecd5e29f7ace3d9d66f0779d6c33487
Sitirohmah06 <mahmnz06>
2022-07-05 14:01:38 +0700

    menambahkan file praktikum 8

--- praktikum_6/application/config/config.php
+++ praktikum_6/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://' . $_SERVER['HTTP_HOST'] . '/pemweb2_muhammadfikri/praktikum6-ci/';
+$config['base_url'] = 'http://' . $_SERVER['HTTP_HOST'] . '/Praktikum-Web-2_Sitirohmah06/praktikum6-ci/';
 
 /*
 |--------------------------------------------------------------------------
